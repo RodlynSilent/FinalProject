5522b20fb4ccfa7001486e0f4f6f14c8dd6e7328
Rodlyn Mahilum <123444084+RodlynSilent@users.noreply.github.com>
2023-04-28 22:33:23 +0800

    Add files via upload

---
+++ Mahilum_Final_Project/src/main/java/com/csit228g3/mahilum/mahilum_final_project/Mahilum_Final_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.csit228g3.mahilum.mahilum_final_project;
+
+/**
+ *
+ * @author Rodlyn Mahilum
+ */
+public class Mahilum_Final_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Mahilum_Final_Project/src/main/java/com/csit228g3/mahilum/mahilum_final_project/MySQLConnection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csit228g3.mahilum.mahilum_final_project;
+
+/**
+ *
+ * @author karin
+ */
+public class MySQLConnection {
+    
+}
